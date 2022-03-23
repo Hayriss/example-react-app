6b100d69259ef2cb4d697d17856636df30406b19
Hayriss <bhayriye93@gmail.com>
2022-03-23 16:34:46 +0300

    testing commit

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <>
       <main>
         <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
+        <p>That feels like an existential question, don't you think ?</p>
       </main>
     </>
   );
